6d479a00f5a3098ae41e1cf6746929c949b3a73e
Ângelo Gabriel <gabrielpalhano.dev@gmail.com>
2023-03-23 18:18:11 -0300

    Teste.att
    Teste.att; do while vs while.

--- teste/Teste.java
+++ teste/Teste.java
@@ -1,12 +1,15 @@
 class Teste {
-	public static void main (String[] args) {
-		double dist = 5;
-		double cons = 5;
-		double vlrLitro = 5;
-		double vlrGasto = vlrLitro*cons;
-		
-		
-		
-		System.out.println("Ao percorrer: " + dist + " Km/h, \nvocê terá gasto: " + (String.format("%.2f", vlrGasto)) + " reais!");
+	public static void main(String[] args) {
+		int i = 1;
+		int j = 1;
+			do{
+				System.out.println(j);
+				i = i+1;
+			} while(i < 1);
+			
+			while(j < 1) {
+				System.out.println(j);
+				j = j+1;
+			}
 	}
 }
